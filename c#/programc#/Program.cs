765b18c4f4e2f2421d5c6b9e655a60e820f009d4
JBiswin <163123735+JBiswin@users.noreply.github.com>
2024-05-13 15:38:41 +0900

    2024.5.13(변수의선언)

---
+++ c#/programc#/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace programc_
+{
+    #region 클래스
+    //사용자 정의 데이터 유형으로 속성과 함수가 포함되어 있으며,
+    //클래스를 통해 객체를 생성하여 접근하고 사용할 수 있는 집합체입니다.
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 변수
+            //데이터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
+
+            //  bool flag = true;
+            //  
+            //  char alphabet = 'A';
+            //  
+            //  int data;
+            //  
+            //  float pi = 3.14f;
+            //  
+            //  double health = 123.3;
+            //  
+            //  data = 100;
+            //  
+            //  Console.WriteLine("flag 변수의 값:" + flag);
+            //  Console.WriteLine("alphabet 변수의 값:" + alphabet);
+            //  Console.WriteLine("data 변수의 값:" + data);
+            //  Console.WriteLine("float pi 변수의 값:" + pi);
+            //  Console.WriteLine("double health 변수의 값:" + health);
+            //변수의 메모리 공간은 프로그램이 실행될 때마다 바뀌며,
+            //여러개의 변수가 있을때 서로 고유의 메모리 공간을 가지고 있습니다.
+
+            //메모리 공간으로 해당 메모리에 저장된 값을 참조할 수 있으며,
+            //변수를 생성하기 위해 메모리 공간의 크기를 지정해주는 자료형을 선언해야 합니다.
+            #region 자료형
+            //데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+            //자료형은 각각의 크기가 정해져 있으며 자료형의 크기는 바이트 단위로 이루어져 있으며
+            //자료형의 경우 자료형에 따라 저장할수 있는 값의 종류와 범위가 결정되어 있습니다.
+
+
+
+
+            #endregion
+            #endregion
+            //main함수는 프로그램의 진입점 역할을 수행하는 함수입니다.
+
+        }
+    }
+}
