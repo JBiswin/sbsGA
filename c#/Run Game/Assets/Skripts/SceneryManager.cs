a692416955fe9793301a2c51385fd85ce87d66f6
JBiswin <163123735+JBiswin@users.noreply.github.com>
2024-09-11 15:43:37 +0900

    2024.9.11

---
+++ c#/Run Game/Assets/Skripts/SceneryManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneryManager : MonoBehaviour
+{
+    [SerializeField] Image screenImage;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
