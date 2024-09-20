c75d797514ad611582027b5362afdabaa747a7c4
JBiswin <163123735+JBiswin@users.noreply.github.com>
2024-09-20 15:37:26 +0900

    2024.9.20

--- c#/Run Game/Assets/Skripts/managers/coinmanager.cs
+++ c#/Run Game/Assets/Skripts/managers/coinmanager.cs
@@ -7,6 +7,7 @@ public class coinmanager : MonoBehaviour
     [SerializeField] GameObject prefab;
     [SerializeField] float offset = 2.5f;
     [SerializeField] int ceratecount = 16;
+    [SerializeField] int positionX = 0;
     void Awake()
     {
         Create();
@@ -16,13 +17,24 @@ public class coinmanager : MonoBehaviour
     {
         for(int i =0;  i < ceratecount; i++)
         {
-            prefab = Instantiate(prefab);
-            prefab.transform.SetParent(gameObject.transform);
-            prefab.transform.localPosition = new Vector3(0, 0, offset * i);
+            GameObject clone = Instantiate(prefab);
+            clone.transform.SetParent(gameObject.transform);
+            clone.transform.localPosition = new Vector3(0, 0, offset * i);
+            int index = clone.name.IndexOf("(Clone)");
+            clone.SetActive(false);
+            if(index >0)
+            {
+                clone.name = clone.name.Substring(0, index);
+            }
+            
+
         }
 
         
     }
 
-    
+    public void InitializePosition()
+    {
+        transform.localPosition = new Vector3(positionX*Random.Range(-1,2),0, 0);
+    }
 }
